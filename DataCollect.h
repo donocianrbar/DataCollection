883958b81ca667c23c6e2ad25e916268a75cc0fb
donocianrbar <shuiwosong19911106@126.com>
2021-04-11 21:57:40 +0800

    更新 DataCollect.h

--- DataCollect.h
+++ DataCollect.h
@@ -1,5 +1,5 @@
-#ifndef DATACOLLECT_H
-#define DATACOLLECT_H
+#ifndef DataCollect_hpp
+#define DataCollect_hpp
 
 #include <iostream>
 #include "opencv2/opencv.hpp"
@@ -7,65 +7,66 @@
 using namespace cv;
 using namespace std;
 
-#define DATA_BUFF_SIZE_MAX 2000
+#define DATA_BUFF_ROW_SIZE_MAX 5
+#define DATA_BUFF_COL_SIZE_MAX 1200
 
 class DataCollect
 {
 public:
     DataCollect(int canvas_w,int canvas_h,int ndataSize,int ndataRange);
-
-	/**
+    
+    /**
      * @brief  调用私有方法dataCollect，通过多次调用，传入多个数据，在一个窗口绘制该组数据的波形
      * @param  data    显示的数据向量
      * @param  color   对应的颜色向量
-	 * @param  winname 窗口名
-	 * @return 
+     * @param  winname 窗口名
+     * @return
      * @autor  参与开发的人员
-	 * @date   2018-11-14 
+     * @date   2018-11-14
      */
-	void dataCollectProc(vector<float> data,vector<Scalar> color,const String winname);
-	
-	/**
+    void dataCollectProc(vector<float> data,vector<Scalar> color,const String winname);
+    
+    /**
      * @brief  调用私有方法dataCollect，通过传入一组数据，在一个窗口绘制该组数据的波形
      * @param  data    显示的数据
      * @param  color   对应的颜色
-	 * @param  winname 窗口名
-	 * @return 
+     * @param  winname 窗口名
+     * @return
      * @autor  参与开发的人员
-	 * @date   2018-11-14 
+     * @date   2018-11-14
      */
-	void dataCollectInvProc(float data,Scalar color,const String winname);
-	
-	/**
+    void dataCollectInvProc(float data,Scalar color,const String winname);
+    
+    /**
      * @brief  私有方法，实现核心的数据波形绘制操作
      * @param  data    显示的数据
      * @param  color   对应的颜色
-	 * @param  winname 窗口名
-	 * @return 
+     * @param  winname 窗口名
+     * @return
      * @autor  参与开发的人员
-	 * @date   2018-11-14 
+     * @date   2018-11-14
      */
-    void dataCollect(float data,Scalar color,const String winname);
-	
-	/**
-     * @brief  
+    void dataCollect(float data,Scalar color,int data_q_index,const String winname);
+    
+    /**
+     * @brief
      * @param  data    显示的数据
      * @param  color   对应的颜色
-	 * @return 
+     * @return
      * @autor  参与开发的人员
-	 * @date   2018-11-14 
+     * @date   2018-11-14
      */
-    void gui(float data,Scalar color);
-	
-	/**
+    void gui();
+    
+    /**
      * @brief  设置画布
-	 * @return 
+     * @return
      * @autor  参与开发的人员
-	 * @date   2018-11-14 
+     * @date   2018-11-14
      */
     void setCanvas();
 private:
-    float data_q[DATA_BUFF_SIZE_MAX];//数据队列
+    float data_q[DATA_BUFF_ROW_SIZE_MAX][DATA_BUFF_COL_SIZE_MAX];//数据队列
     int data_q_h;
     int canvas_w;//画布宽度
     int ndataSize;//显示数据个数
@@ -73,12 +74,12 @@ private:
     float interval;//区间=画布宽度/显示数据个数
     int canvas_h;//画布高度
     Mat coll;//画布
-
+    
     float proportion_v;//值比例
     float proportion_t;//时间比例
-
+    
     int offset_v;//值偏移
     int offset_t;//时间偏移
 };
 
-#endif // DATACOLLECT_H
+#endif /* DataCollect_hpp */
